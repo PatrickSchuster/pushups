a36ea55b576f4db57245954938e3cc91c7e1d4d0
Patrick <patrick.schustermail@gmail.com>
2023-02-21 13:18:02 +0100

    Simplify, add localstorage to keep track of best score

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const toggleBtn = document.getElementById("toggleBtn")
 const count = document.getElementById("count")
 const rectangle = document.getElementById("rectangle")
-const achievementText = document.getElementById("achievement-text")
-const countDownText = document.getElementById("count-down")
+const prevBest = document.getElementById("previous-best")
 
 // Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=42285">Pixabay</a>
 const sound = new Audio("switch.mp3");
-const PREPARE_DURATION = 3000
 const PUSHUP_DURATION = 2000
 
 let toggled = false
 let preparationInterval
 let pushupInterval
 
-let countDown = 3
 let counter = 0
 
-document.body.addEventListener("mousemove", (event) => { 
+prevBest.textContent = localStorage.getItem("score") ?? "0"
+
+document.body.addEventListener("mousemove", event => { 
   const { clientX, clientY } = event
   rectangle.animate({
     left: `${clientX}px`,
@@ -34,49 +33,29 @@ toggleBtn.addEventListener("click", () => toggle())
 
 function toggle() {
     toggled = !toggled
-    toggled === true ? startCountDown() : stop()
+    toggled === true ? start() : stop()
 }
 
 function stop() {
     toggleBtn.innerText = "START"
     clearInterval(preparationInterval)
     clearInterval(pushupInterval)
-    achievementText.style.visibility = "visible"
-    console.log("counter = ", counter)
-    if (counter <= 1) {
-        achievementText.style.visibility = "hidden"
-    } else {
-        achievementText.innerText = "You achieved"
-    }
-    countDown = 3
-    countDownText.textContent = countDown
-    countDownText.style.visibility = "hidden"
+    storePushupScore()
 }
 
-function startCountDown() {
+function start() {
     toggleBtn.innerText = "STOP"
-    count.style.visibility = "hidden"
-    achievementText.style.visibility = "hidden"
-    counter = 0
-    count.innerText = counter
-    countDownText.style.visibility = "visible"
-    preparationInterval = setInterval(() => {
-        countDownText.textContent = countDown
-        if (countDown === 0) {
-            clearInterval(preparationInterval)
-            startPushupsCounter()
-        }
-        countDown--
-    }, 1000)
-}
-
-function startPushupsCounter() {
-    countDownText.style.visibility = "hidden"
-    count.style.visibility = "visible"
+    count.textContent = "0"
     counter = 1
     pushupInterval = setInterval(() => {
         sound.play()
-        count.textContent = counter  
+        count.textContent = counter
         counter++
     }, PUSHUP_DURATION)
+}
+
+function storePushupScore() {
+    if (counter > localStorage.getItem("score")) {
+        localStorage.setItem("score", counter-1)
+    }
 }
